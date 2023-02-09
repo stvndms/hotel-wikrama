8282e3e5d36c84e3a38ce4f7ae411c180475d87d
fadhiilabiyyi <fadhiilabiyyitamsil007@gmail.com>
2023-02-09 10:11:57 +0700

    CRUD Data Guest API

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Guest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class GuestController extends Controller
 {
@@ -14,7 +15,8 @@ class GuestController extends Controller
      */
     public function index()
     {
-        //
+        $guests = Guest::all();
+        return response()->json(['guests' => $guests], 200);
     }
 
     /**
@@ -96,5 +98,59 @@ class GuestController extends Controller
     public function destroy(Guest $guest)
     {
         //
+    }
+
+    public function createApi(Request $request) 
+    {
+        $rules = [
+            'guest_name' => ['required', 'max:255'],
+            'guest_photo' => ['image', 'file', 'max:3024'],
+            'guest_phone' => ['required'],
+            'guest_country' => ['required'],
+            'guest_address' => ['required'],
+            'guest_id_card' => ['image', 'file', 'max:3024']
+        ];
+
+        $validatedData = $request->validate($rules);
+        // $validatedData['guest_photo'] = $request->file('guest_photo')->store('uploaded-images');
+        // $validatedData0['guest_id_card'] = $request->file('guest_id_card')->store('uploaded-images');
+
+        Guest::create($validatedData);
+
+        return response()->json(
+            [
+                'guest' => $validatedData
+            ], 201);
+    }
+
+    public function updateApi(Request $request, Guest $guest)
+    {
+        $rules = [
+            'guest_name' => ['required', 'max:255'],
+            'guest_photo' => ['image', 'file', 'max:3024'],
+            'guest_phone' => ['required'],
+            'guest_country' => ['required'],
+            'guest_address' => ['required'],
+            'guest_id_card' => ['image', 'file', 'max:3024']
+        ];
+
+        $validatedData = $request->validate($rules);
+        Guest::where('id', $guest->id)->update($validatedData);
+
+        return response()->json(
+            [
+                'guest' => $validatedData
+            ], 200); 
+    }
+
+    public function deleteApi(Guest $guest) {
+        Guest::destroy($guest->id);
+        $date = Carbon::now()->toDateTimeString();
+        return response()->json([
+            'message' => 'Data tamu sudah dihapus',
+            'status' => '200',
+            'reason' => 'OK',
+            'timestamp' => $date
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GuestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::resource('guests', GuestController::class);
+Route::get('/guest', [GuestController::class, 'index']);
+Route::post('/guest', [GuestController::class, 'createApi']);
+Route::post('/guest/update/{guest:id}', [GuestController::class, 'updateApi']);
+Route::delete('/guest/delete/{guest:id}', [GuestController::class, 'deleteApi']);
