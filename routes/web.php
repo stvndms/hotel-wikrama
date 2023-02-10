3a2ed2d5c2e5ab83b25d879f696325290456c7c0
= <novavalentino925@gmail.com>
2023-02-10 09:33:46 +0700

    Update routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\AdminUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Login Routes
+Route::get('/login', [LoginController::class, 'login'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+        
+// Register Routes
+Route::get('/register', [RegisterController::class, 'register'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->name('store')->middleware('guest');
+
+// Dashboard Routes
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
 Route::get('/', function () {
-    return view('login.register');
+    return view('landing.index');
+})->middleware('guest');
+
+Route::get('/home', function () {
+    return view('landing.index');
 });
